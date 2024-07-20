f6ebb984c9ed0ed11bab9e3bc840735223574bcc
Rebecca <rebecca90469@gmail.com>
2024-07-20 14:43:26 -0400

    Testing out db setup

---
+++ server/src/db/schema/seed.tables.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS (ID, NAME, EMAIL, PASSWORD) 
+VALUES 
+(1,'Alice', 'alice@wonderland.ca', 'password'),
+(2,'Bob', 'bob@burger.ca', 'password'),
+(3,'Charlie', 'charlie@chocolate.ca', 'password');
