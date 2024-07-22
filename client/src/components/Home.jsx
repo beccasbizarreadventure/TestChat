1b2cdbea76383949c4a0a42a61a15b413422b4e4
Rebecca <rebecca90469@gmail.com>
2024-07-22 00:00:14 -0400

    Some frontend started and tailwind installed

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,3 @@
-import React from "react"
-import Home from "./components/Home"
-import Login from "./components/Login"
-import Register from "./components/Register"
-import { Route, Routes } from "react-router-dom";
-
 function App() {
 
   return (
